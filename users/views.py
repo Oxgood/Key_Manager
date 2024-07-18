facbb7bce2c085a13ef1f06dfa498b8ff9159c55
Oxgood Atta-Agyekum <agyekumoxgood@gmail.com>
2024-07-18 22:30:49 +0000

    initial commit

---
+++ users/views.py
@@ -0,0 +1,55 @@
+from users.register import RegisterForm
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth import get_user_model
+
+
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            token = default_token_generator.make_token(user)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            confirmation_link = request.build_absolute_uri(f'/confirm-email/{uid}/{token}/')
+            html_message = render_to_string('users/confirmation_email.html', {'confirmation_link': confirmation_link, 'user': user.email})
+            send_mail(
+                'Confirm your email',
+                'Please confirm your email by clicking the link below.',
+                'agyekumoxgood@gmail.com',
+                [user.email],
+                html_message=html_message
+            )
+            return redirect('email_confirmation_sent')
+    else:
+        form = RegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+
+
+def confirm_email(request, uidb64, token):
+    user = get_user_model()
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = user.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return redirect('email_confirmed')
+    else:
+        return render(request, 'email_confirmation_invalid.html')
